dad06d0f4126c8bda41168ed3b1a79b3b303135b
Shoxrux Yuldashov <deagleshox@mail.ru>
2024-11-25 16:36:44 +0300

    dev | Генератор случайных книг
    - Добавлена возможность генерации случайных данных перед началом работы с приложением

---
+++ core/managers/random_data_generate.py
@@ -0,0 +1,38 @@
+from typing import Optional
+
+from faker import Faker
+
+__all__ = ["RandomDataGenerator"]
+
+
+class RandomDataGenerator:
+    """
+    Генерации случайных книг и заполнения базы.
+    """
+
+    _DEFAULT_RANDOM_BOOKS_COUNT: int = 10
+
+    def __init__(
+        self,
+        manager: "BookManager",
+        books_count: Optional[int] = None,
+    ) -> None:
+        """
+        :param manager: Объект менеджера книг.
+        :param books_count: Количество случайных книг. По умолчанию 10.
+        """
+        self.manager = manager
+        self.books_count = books_count or self._DEFAULT_RANDOM_BOOKS_COUNT
+        self.faker = Faker()
+
+    def generate_books(self) -> None:
+        """Генерирует список случайных книг."""
+        [
+            self.manager.create(
+                title=self.faker.company(),
+                author=self.faker.name(),
+                year=int(self.faker.year()),
+            )
+            for _ in range(self.books_count)
+        ]
+        print(f"База данных заполнена {self.books_count} случайными книгами")
